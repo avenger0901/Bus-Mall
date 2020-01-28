db5936da13425d14e3b79970fdb216fcee001c15
zhao ziyi <zhaoziy@oregonstate.edu>
2020-01-27 16:58:22 -0800

    add data

---
+++ data/product_data.js
@@ -0,0 +1,86 @@
+export const productData = [{
+    id: 'bag',
+    Name:'Bag',
+    image:'../assets/bag.jpg'
+},
+{
+    id: 'banana',
+    Name:'Banana',
+    image:'../assets/banana.jpg'
+},
+{
+    id: 'bathroom',
+    Name:'Bathroom',
+    image:'../assets/bathroom.jpg'
+},
+{
+    id: 'boots',
+    Name:'Boots',
+    image:'../assets/boots.jpg'
+},
+{
+    id: 'breakfast',
+    Name:'Breakfast',
+    image:'../assets/breakfast.jpg'
+},
+{
+    id: 'bubblegum',
+    Name:'Bubblegum',
+    image:'../assets/bubblegum.jpg'
+},
+{
+    id: 'chair',
+    Name:'Chair',
+    image:'../assets/chair.jpg'
+},
+{
+    id: 'cthuhu',
+    Name:'Cthuhu',
+    image:'../assets/ctuhu.jpg'
+},
+{
+    id: 'dog-duck',
+    Name:'Dog-duck',
+    image:'../assets/dog-duck.jpg'
+},
+{
+    id: 'dragon',
+    Name:'Dragon',
+    image:'../assets/dragon.jpg'
+},
+{
+    id: 'pen',
+    Name:'Pen',
+    image:'../assets/pen.jpg'
+},
+{
+    id: 'pet-sweep',
+    Name:'Pet-sweep',
+    image:'../assets/pet-sweep.jpg'
+},
+{
+    id: 'scissors',
+    Name:'Scissors',
+    image:'../assets/scissors.jpg'
+},
+{
+    id: 'shark',
+    Name:'Shark',
+    image:'../assets/shark.jpg'
+},
+{
+    id: 'sweep',
+    Name:'Sweep',
+    image:'../assets/sweep.jpg'
+},
+{
+    id: 'tauntaun',
+    Name:'Tauntaun',
+    image:'../assets/tauntaun.jpg'
+},
+
+
+
+
+
+];
